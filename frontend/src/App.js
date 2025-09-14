bb629c07c5581b3a23c369ee81a288b6051e3473
Raphael Almeida Pascon <101514999+baconzin@users.noreply.github.com>
2025-09-13 23:01:26 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import { Toaster } from "./components/ui/sonner";
+import FloatingWhatsapp from "./components/FloatingWhatsapp"; // ⬅️ importe o botão
 
 function App() {
   return (
@@ -11,7 +12,12 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
         </Routes>
+
+        {/* Botão flutuante do WhatsApp visível em todas as páginas */}
+        <FloatingWhatsapp />
       </BrowserRouter>
+
+      {/* Toast notifications */}
       <Toaster />
     </div>
   );
